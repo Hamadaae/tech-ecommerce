69a6154148b0b5be688131922d2986f28f160c18
Hamada <gunslinger12398@gmail.com>
2025-11-11 00:19:20 +0200

    test3

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-(async () => {
-    try{
-        await connectDB();
-        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    } catch (error) {
-        console.error('Failed to start', error);
-        process.exit(1)
-    }
-})();
+await mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+});
